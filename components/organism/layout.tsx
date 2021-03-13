2390f58794ac8e5eb18a7afedaa4dfaa40c3d935
michael eze <Michael.eze@ext.elli.eco>
2021-03-13 23:46:09 +0100

    the begining

---
+++ components/organism/layout.tsx
@@ -0,0 +1,16 @@
+import Alert from '../molecule/alert'
+import Footer from '../molecule/footer'
+import Meta from '../molecule/meta'
+
+export default function Layout({ preview, children }) {
+  return (
+    <>
+      <Meta />
+      <div className="min-h-screen">
+        <Alert preview={preview} />
+        <main>{children}</main>
+      </div>
+      <Footer />
+    </>
+  )
+}
